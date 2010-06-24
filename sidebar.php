e02f7e459df6eb7437cbf033e5143ff669111cc9
Daniel Karlsson <daniel@fraction4.com>
2010-06-24 15:46:35 +0200

    Made lists smaller in the sidebar.

--- sidebar.php
+++ sidebar.php
@@ -3,11 +3,11 @@
 	<div class="space10">
 		<?php get_search_form(); ?>
 		<h2>Recent Posts</h2>
-		<ul>
+		<ul class="small">
 			<?php wp_get_archives('type=postbypost&limit=5'); ?>  
           </ul>
 		<h2>Meta</h2>
-		<ul>
+		<ul class="small">
 			<li><a href="<?php bloginfo('rss2_url'); ?>">Posts feed</a></li>
 			<li><a href="<?php bloginfo('comments_rss2_url'); ?>">Comments feed</a></li>
 			<li><?php wp_loginout(); ?></li>
@@ -18,12 +18,12 @@
 <div style="float:left; width:207px; margin-left:11px;">
 	<p>&nbsp;</p>
 	<h2>Categories</h2>
-	<ul>
+	<ul class="small">
 		<?php wp_list_categories('hide_empty=1&show_count=1&depth=1&title_li='); ?>
 	</ul>
 	
 	<h2>Archives</h2>
-	<ul>
+	<ul class="small">
 		<?php wp_get_archives('type=monthly&show_post_count=1'); ?>
 	</ul>
 	
