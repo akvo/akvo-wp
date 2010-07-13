834e8499ea384b20113863f5564118e4df40e958
Daniel Karlsson <daniel@fraction4.com>
2010-07-13 15:16:58 +0200

    Updated the footer links

--- footer.php
+++ footer.php
@@ -4,58 +4,59 @@
 	<div id="footer_container" class="container">
 		<div class="span-18">
 			<h2>About Akvo.org</h2>
+			<p>
+				We help donors and doers reach out to fund many thousands of new water and sanitation projects. Money flows quickly because donors choose what to fund and follow progress online. People can use these storylines to build exciting new campaigns and networks. Our partners lead the world in sharing updates using Akvo tools. Better feedback means happy donors, and dialogue between field workers builds skills and improves quality. Time and money are saved while people across the world get safe drinking water and proper sanitation.
+			</p>
 		</div>
 		<div class="span-18">
 			<div class="span-3 first">
 				<h3>Policies</h3>
 				<p class="small">
-					<a href="#">Privacy</a><br />
-					<a href="#">Legal</a><br />
-					<a href="#">Partners</a><br />
-					<a href="#">Donations</a><br />
-					<a href="#">Submissions</a>
+					<a href="/web/terms_of_use">Terms of use</a><br />
+					<a href="/web/donation_policy">Donations</a><br />
+					<a href="/web/partners_and_projects_policy">Partners</a><br />
+					<a href="/web/user_contribution_policy">User Contribution Policy</a><br />
+					<a href="/web/open_license">Akvo licensing and copyrights</a><br />
+					<a href="/web/privacy_policy">Privacy</a><br />
+					<a href="/web/akvo-rsr-api-code-of-conduct">Akvo RSR API Code of Conduct</a><br />
 				</p>
 			</div>
 			<div class="span-3">
 				<h3>About</h3>
 				<p class="small">
-					<a href="#">Read our blog</a><br />
-					<a href="#">Who are we?</a><br />
-					<a href="#">How does it work</a><br />
-					<a href="#">Financials</a><br />
-					<a href="#">Statistics</a><br />
-					<a href="#">FAQs</a><br />
-					<a href="#">Thanks</a><br />
-					<a href="#">Jobs</a><br />
+					<a href="/web/what_we_do">What we do</a><br />
+					<a href="/web/how_akvo_works">How Akvo works</a><br />
+					<a href="/web/why_we_do_it">Why we do it</a><br />
+					<a href="/web/faq">Akvo Frequently Asked Questions</a><br />
+					<a href="/web/map_partners_projects">Map</a><br />
+					<a href="/web/team">Team</a><br />
+					<a href="/web/jobs">Working at Akvo</a><br />
+					<a href="/web/press">Press</a><br />
+					<a href="/web/annual_reports">Akvo annual reports & Articles of incorporation</a><br />
 				</p>
 			</div>
 			<div class="span-3">
 				<h3>Partners</h3>
 				<p class="small">
-					<a href="#">Professionals</a><br />
-					<a href="#">Organisations</a><br />
-					<a href="#">Corporate</a><br />
-					<a href="#">Volunteers</a><br />
-					<a href="#">Knowledge</a><br />
+					<a href="http://akvo.org/web/partners">Strategic partners</a><br />
+					<a href="{% url rsr_org_list %}">Project partners</a><br />
+					<a href="/web/commercial_partners">Commercial partners</a><br />
+					<a href="/web/knowledge_institute_partners">Knowledge institute partners</a><br />
+					<a href="/web/akvo_investors">Akvo investors</a><br />
 				</p>
 			</div>
 			<div class="span-3">
 				<h3>Developers</h3>
 				<p class="small">
-					<a href="#">Akvo Labs</a><br />
-					<a href="#">Developer Guide</a><br />
-					<a href="#">Source Code</a><br />
+					<a href="/labs/index.php/Main_Page">Akvo Labs</a><br />
+					<a href="http://github.com/akvo/">Source Code</a><br />
 				</p>
 			</div>
 			<div class="span-3">
 				<h3>Contact us</h3>
 				<p class="small">
 					<a href="http://help.akvo.org/">Help &amp; Support</a><br />
-					<a href="#">Website Feedback</a><br />
-					<a href="#">General Questions</a><br />
-					<a href="#">Press Inquiries</a><br />
-					<a href="#">Mailing Address</a><br />
-					<a href="#">Directions</a><br />
+					<a href="/web/contact_us">Contact us</a><br />
 				</p>
 			</div>
 			<div class="span-3">
@@ -68,14 +69,16 @@
 	</div>
 </div>
 	
+<?php /*
+
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
 <script type="text/javascript">
 	var $ = jQ = jQuery.noConflict();
 	
 	jQ(document).ready(function(){
 		
-	
 	});	
-</script>	
+</script>
+*/ ?>	
 </body>
 </html>
